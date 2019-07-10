e9f9d3252c05640c302d4dbec22e7cde3c0d7b23
“Gabriel <g.olivieri@swascan.com>
2019-07-10 09:28:02 -0400

    Project commit

---
+++ app/src/main/java/cembi/ruedalaapp/BiciescuelasActivity.java
@@ -0,0 +1,13 @@
+package cembi.ruedalaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BiciescuelasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biciescuelas);
+    }
+}
